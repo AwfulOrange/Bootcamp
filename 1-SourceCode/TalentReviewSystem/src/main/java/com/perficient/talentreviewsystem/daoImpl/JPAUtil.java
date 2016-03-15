89f6ecf38e49d2ecd88da852dd2d6871a056f9b3
Yuhang.Zhang <yuhang.zhang@perficient.com>
2016-03-15 18:09:41 +0800

    review holiday xia

---
+++ 1-SourceCode/TalentReviewSystem/src/main/java/com/perficient/talentreviewsystem/daoImpl/JPAUtil.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.perficient.talentreviewsystem.daoImpl;
+
+/**
+ *
+ * @author bootcamp19
+ */
+public class JPAUtil {
+    public static final String JPA ="com.perficient_TalentReviewSystem_war_1.0-SNAPSHOTPU";
+}

d18f56a1b35d2c2efb3bdde174842ebdf7998523
Clover <clover.yao@perficient.com>
2016-03-17 20:15:51 +0800

    validate submit button

--- 1-SourceCode/TalentReviewSystem/src/main/webapp/js/myUsers.js
+++ 1-SourceCode/TalentReviewSystem/src/main/webapp/js/myUsers.js
@@ -1,9 +1,13 @@
 angular.module('myApp', []).controller('userCtrl', function($scope,$http) {
 
-
+var empslength=0;
 $http.get("http://localhost:8080/TRS/web/employee/")
 .success(function (data) {
     $scope.emps = data;
+    empslength=data.length;
+    console.log(empslength);
+    
+
 });
 var allscore=[];
 $scope.save=function(emid,achievingresults,orgimpact,learningagility,versatility)
@@ -29,31 +33,56 @@ $scope.save=function(emid,achievingresults,orgimpact,learningagility,versatility
             
         }
     }
+    
     console.log(allscore);
 }
+    var valid=function(){
+//        var a=true; 
+console.log(empslength);
+console.log(allscore.length);
+console.log(allscore.length==empslength);
+        if(allscore.length!=empslength)
+        {
+            return false;       
+        }
+        for(var i=0;i<allscore.length;i++)
+        {
+            for(var j=0;j<5;j++)
+            {
+                console.log(allscore[i].achievingresults==undefined);
+                if(allscore[i].employeeId==undefined){
+                    return false; 
+                }else if(allscore[i].achievingresults==undefined){
+                    return false; 
+                }else if(allscore[i].orgimpact==undefined){
+                    return false; 
+                }else if(allscore[i].learningagility==undefined){
+                    return false; 
+                }else if(allscore[i].versatility==undefined){
+                    return false; 
+                }
+
+            }
+        }
+        return true;
+    }
 
 $scope.postSubmit = function(){
-        console.log(allscore);
-//        var judgena=function(){           
-//                     for(var i=0;i<allscore.length;i++) 
-//                        if(allscore[i])
-//                    {
-//                        
-//                    }
-//        }
-//        if(     1    ) 
-//        {
-//                $http.post('http://localhost:8080/TRS/web/employee/', allscore).success(function()
-//                {
-////           alert(data.employeeId+","+data.achievingresults+","+data.orgimpact+","+data.learningagility+","+data.versatility);
-//                })
-//        }
-//        else
-//        {
-//                 alert("Please check message!");
-//         }
-// 
-    };
+        if(valid()){
+             $http.post('http://localhost:8080/TRS/web/employee/', allscore).success(function(){
+           
+            alert("success");
+           
+        }).error(function(data) {
+            alert("failure message:" + JSON.stringify({data:data}));
+        });
+        }
+        else
+        { alert("please input all data!");}
+           
+       
+};
+
  $scope.number=[1,2,3,4,5 ];
 
 });

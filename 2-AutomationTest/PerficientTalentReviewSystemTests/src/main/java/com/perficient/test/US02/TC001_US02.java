e4e94f4330eadc625cdc11f436734477a91bb14d
Hedy.Ru <hedy.ru@perficient.com>
2016-03-21 18:53:02 +0800

    user2

--- 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US02/TC001_US02.java
+++ 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US02/TC001_US02.java
@@ -8,20 +8,15 @@ package com.perficient.test.US02;
 
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.ScorePage;
 import static pages.ScorePage.mouseoverElement1;
 import static pages.ScorePage.mouseoverElement2;
 import static pages.ScorePage.mouseoverElement3;
 import static pages.ScorePage.mouseoverElement4;
+import util.TestCaseBase;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -33,29 +28,21 @@ import static pages.ScorePage.mouseoverElement4;
  *
  * @author bootcamp19
  */
-public class TC001_US02 {
-    public static WebDriver driver; 
+public class TC001_US02 extends TestCaseBase{
+   // public static WebDriver driver; 
+    //public static final String URL="http://10.2.1.183:8080/TRS/page/score.jsp";
     public static final String URL="http://localhost:8080/TRS/page/score.jsp";
     public static ScorePage sp=new ScorePage();
-    @BeforeMethod
-    public static void setupFirefox() throws Exception{
-        driver = new FirefoxDriver();
-        PageFactory.initElements(driver, sp);
-       
-    }
-    
-//    public static void click(){
-//       driver.navigate().to(URL);
-//       sp.clickElement.click();
-//    }
+   
     @Test
     public static void click() throws InterruptedException{
-        driver.navigate().to(URL);
+         PageFactory.initElements(TestCaseBase.driver, sp); 
+        TestCaseBase.driver.navigate().to(URL);
         Thread.sleep(2000);
         sp.clickElement.click();
         Thread.sleep(2000);
         
-        Actions action = new Actions(driver);
+        Actions action = new Actions(TestCaseBase.driver);
         action.moveToElement(mouseoverElement1).perform();
         Thread.sleep(2000);
          action.moveToElement(mouseoverElement2).perform();
@@ -67,22 +54,7 @@ public class TC001_US02 {
 
     }
     
+
    
-//     public static void mouseover() throws InterruptedException{
-//          //driver.navigate().to(URL);
-//       //action.clickAndHold(mouseoverElement);
-//      //  WebElement ele = driver.findElement(By.id("AQ0"));
-//       
-//       //sp.mouseoverElement.click();
-//        
-//       //  String excepttitle="xxxxxxx";
-//        // String truetitle=driver.getTitle();
-//        // assertEquals(excepttitle,truetitle);
-//     }
-    @AfterMethod
-     public static void closeFirefox() throws Exception
-    {
-        driver.quit();
-    }
 }
 

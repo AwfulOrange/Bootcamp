7916610b37ae2b9dbe07f314522fec2af3793358
candice.jin <candice.jin@perficient.com>
2016-03-21 14:47:38 +0800

    new automatical code

--- 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US01/TC001_US01.java
+++ 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US01/TC001_US01.java
@@ -6,7 +6,7 @@
 package com.perficient.test.US01;
 
 
-
+import static com.perficient.test.US02.US01_TC001.driver;
 import java.util.ArrayList;
 import java.util.List;
 import org.openqa.selenium.By;
@@ -45,16 +45,17 @@ public class TC001_US01 {
     
     @Test
     public static void click() throws InterruptedException{
+        //---------------------1---------------------//
         driver.navigate().to(URL);
         Thread.sleep(2000);
         sp.clickElement.click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         sp.click2Element.click(); 
         Thread.sleep(1000);
+        sp.click2Element.click(); 
         sp.click3Element.click(); 
-        sp.click4Element.click(); 
         Thread.sleep(1000);
-        sp.click5Element.click();   
+        sp.click3Element.click();   
         Thread.sleep(1000);
         for(int r=65;r<69;r++){
             WebElement select=driver.findElement(By.xpath("//select[@id='"+(char)r+"0']"));
@@ -63,9 +64,61 @@ public class TC001_US01 {
                 select.findElement(By.cssSelector("option[label='"+Integer.toString(j)+"']")).click();
             }
         }
-        sp.click6Element.click();
+        for(int s=69;s<73;s++)
+        {
+            driver.findElement(By.xpath("//input[@id='"+(char)s+"0']")).sendKeys("NA");
+        }
+        sp.clickElement.click();
+        Thread.sleep(1000);
+        //---------------------2---------------------//
+        sp.click4Element.click();
+        Thread.sleep(1000);
+        sp.click5Element.click(); 
+        Thread.sleep(1000);
+        sp.click5Element.click(); 
+        sp.click6Element.click(); 
+        Thread.sleep(1000);
+        sp.click6Element.click();   
+        Thread.sleep(1000);
+        for(int r=65;r<69;r++){
+            WebElement select=driver.findElement(By.xpath("//select[@id='"+(char)r+"1']"));
+            for(int j=1;j<6;j++)
+            {
+                select.findElement(By.cssSelector("option[label='"+Integer.toString(j)+"']")).click();
+            }
+        }
+        for(int s=69;s<73;s++)
+        {
+            driver.findElement(By.xpath("//input[@id='"+(char)s+"1']")).sendKeys("NA");
+        }
+        sp.click4Element.click();
+        Thread.sleep(1000);
+        //---------------------3---------------------//
+        sp.click7Element.click();
+        Thread.sleep(1000);
+        sp.click8Element.click(); 
+        Thread.sleep(1000);
+        sp.click8Element.click(); 
+        sp.click9Element.click(); 
+        Thread.sleep(1000);
+        sp.click9Element.click();   
+        Thread.sleep(1000);
+        for(int r=65;r<69;r++){
+            WebElement select=driver.findElement(By.xpath("//select[@id='"+(char)r+"2']"));
+            for(int j=1;j<6;j++)
+            {
+                select.findElement(By.cssSelector("option[label='"+Integer.toString(j)+"']")).click();
+            }
+        }
+        for(int s=69;s<73;s++)
+        {
+            driver.findElement(By.xpath("//input[@id='"+(char)s+"2']")).sendKeys("NA");
+        } 
+        sp.click7Element.click();
+        Thread.sleep(1000);
+        sp.click10Element.click();
         Thread.sleep(2000);
-    }
+    };
     
     @AfterMethod
      public static void closeFirefox() throws Exception

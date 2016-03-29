e972680ff2a0cc95c2575d94deb1647458f1028b
holiday.xia <holiday.xia@perficient.com>
2016-03-29 10:07:15 +0800

    modify httpconnection

--- 1-SourceCode/TalentReviewSystem/src/main/java/com/perficient/talentreviewsystem/utils/HttpConnection.java
+++ 1-SourceCode/TalentReviewSystem/src/main/java/com/perficient/talentreviewsystem/utils/HttpConnection.java
@@ -6,6 +6,7 @@
 package com.perficient.talentreviewsystem.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -17,12 +18,12 @@ import java.util.logging.Logger;
  * @author bootcamp19
  */
 public class HttpConnection {
-        
-    private HttpConnection(){}
 
-    public  static String getFromUrl(String url) {
-    String result = "";
-    BufferedReader in = null;
+    private HttpConnection() {
+    }
+
+    public static String getFromUrl(String url) {
+        String result = "";
         try {
             URL realUrl = new URL(url);
             URLConnection connection = realUrl.openConnection();
@@ -31,26 +32,23 @@ public class HttpConnection {
             connection.setRequestProperty("user-agent",
                     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             connection.connect();
-            in = new BufferedReader(new InputStreamReader(
-                    connection.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
+            result = readLine(connection, result);
         } catch (Exception e) {
             Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, e);
         }
-        finally {
-            try {
-                if (in != null) 
-                    in.close();
-            } catch (Exception e2) {
-                Logger.getLogger(HttpConnection.class.getName()).log(Level.SEVERE, null, e2);
-            }
-        }
         return result;
-    
+
     }
-      
-        
+
+    private static String readLine(URLConnection connection, String result) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            result += line;
+        }
+        in.close();
+        return result;
+    }
+
 }

7b8b606efb8be3344d66b949098a1e62f15b2b13
candice.jin <candice.jin@perficient.com>
2016-03-30 16:14:38 +0800

    update USTC001

--- 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US05/TC001_US05.java
+++ 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/US05/TC001_US05.java
@@ -7,9 +7,11 @@ package com.perficient.test.US05;
 
 
 import com.perficient.test.pages.LoginPage;
+import com.perficient.test.pages.PMOleadPage;
 import com.perficient.test.pages.ScorePage;
 import org.testng.annotations.Test;
 import com.perficient.test.util.TestCaseBase;
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
@@ -30,73 +32,92 @@ public class TC001_US05 extends TestCaseBase{
     public static final String URL="http://10.2.1.183:8080/TRS/page/score.jsp";
     public static ScorePage sp = new ScorePage();
     public static LoginPage lp = new LoginPage();
+    public static PMOleadPage pp = new PMOleadPage();
     
     @Test
     public static void click() throws InterruptedException{
         PageFactory.initElements(TestCaseBase.driver, sp); 
-        PageFactory.initElements(TestCaseBase.driver, lp); 
-        driver.navigate().to(URL);
-        login("yuhang.zhang","tRApha7T");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("/html/body/header/div/table/tbody/tr/td[2]/a/b")).click();
-        Thread.sleep(2000);
-        for(int i=0;i<4;i++){
-            driver.findElement(By.xpath("//tr[@id='pmo"+i+"']")).click();
-            Thread.sleep(1000);
-           for(int j=1;j<4;j++)
-            { 
-                int r=i+1;
-                driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/table/tbody/tr[2]/td[1]")).click();                                         
-                Thread.sleep(1000);                    
-                driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/div/h[1]/img")).click();
-                Thread.sleep(1000);    
-                driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/div/h[2]/img")).click();
-                Thread.sleep(1000);
-            }
-        }
+        PageFactory.initElements(TestCaseBase.driver, lp);
         
-        for(int i=0;i<4;i++){
+        TestCaseBase.driver.navigate().to(URL);
+        login("testEM","testEM@gdc");
+        Thread.sleep(2000);
+        
+        TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/table/tbody/tr/td[2]/a/b")).click();
+        PageFactory.initElements(TestCaseBase.driver, pp);
+        Thread.sleep(2000);
+        pp.pmo0.click();
+        Thread.sleep(2000);
+        pp.revid00.click();
+        Thread.sleep(2000);
+        pp.personal00.click(); 
+        Thread.sleep(2000);
+        pp.supportive00.click();
+        pp.pmo1.click();
+        Thread.sleep(2000);
+        pp.revid01.click();
+        Thread.sleep(2000);
+        pp.personal01.click(); 
+        Thread.sleep(2000);
+        pp.supportive01.click();
+////
+////
+////        List<WebElement> columnElements = driver.findElements(By.cssSelector("tr"));
+////         int rowNumber = columnElements.size();
+////         System.out.println("---------------------"+rowNumber);
+//
+            pp.pmo2.click();
+           Thread.sleep(5000);
            for(int j=1;j<4;j++)
-            {
-                int r=i+1;
-                WebElement select = TestCaseBase.driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/table/tbody/tr[2]/td[1]"));
-                Assert.assertNotNull(select,"Fail");
-                WebElement select1 = TestCaseBase.driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/div/h[1]/img"));
-                Assert.assertNotNull(select1,"Fail");
-                WebElement select2 = TestCaseBase.driver.findElement(By.xpath("/html/body/div/div/div["+r+"]/form/div["+j+"]/div/h[2]/img"));
-                Assert.assertNotNull(select2,"Fail");
-                
+           { 
+               driver.findElement(By.xpath("/html/body/header/div/div[5]/div/div/div["+j+"]/table/tbody/tr[2]/td[1]")).click();   
+               Thread.sleep(2000);                  
+               driver.findElement(By.xpath("/html/body/header/div/div[5]/div/div/div["+j+"]/div/h[1]/img")).click();
+               Thread.sleep(2000);    
+               driver.findElement(By.xpath("/html/body/header/div/div[5]/div/div/div["+j+"]/div/h[2]/img")).click();
+               Thread.sleep(2000);
+             }
+        //.............................Assert 1//
+         for(int j=1;j<2;j++){
+                 int r=3;
+                 WebElement select = TestCaseBase.driver.findElement(By.xpath(" /html/body/header/div/div["+r+"]/div/div/div["+j+"]/table/tbody/tr[2]/td[1]"));
+                 Assert.assertNotNull(select,"Fail");
+                 WebElement select1 = TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[1]/img"));
+                 Assert.assertNotNull(select1,"Fail");
+                 WebElement select2 = TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[2]/img"));
+                 Assert.assertNotNull(select2,"Fail");
                 Assert.assertEquals(driver.findElement(By.cssSelector("select[disabled]")).getAttribute("disabled"), "true","Fail");
-            }
-        }
-        for(int i=0;i<4;i++){
-            driver.findElement(By.xpath("//tr[@id='pmo"+i+"']")).click();
-            Thread.sleep(1000);
-        } 
-        logout();
-        Thread.sleep(1000);
-        login("young.tao","Weph8zEr");
-        sp.clickElement.click();
-        driver.findElement(By.xpath("//textarea[@id='E0']")).clear();
-        WebElement select=TestCaseBase.driver.findElement(By.xpath("//select[@id='A0']"));
-        select.findElement(By.cssSelector("option[label='1']")).click();
-        driver.findElement(By.xpath("//textarea[@id='E0']")).sendKeys("He is handsome");
-        Thread.sleep(1000);
-        sp.clickElement.click();
-        logout();
-        Thread.sleep(2000);
-        login("yuhang.zhang","tRApha7T");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("/html/body/header/div/table/tbody/tr/td[2]/a/b")).click();
-        driver.findElement(By.xpath("//tr[@id='pmo0']")).click();
-        driver.findElement(By.xpath("/html/body/div/div/div[1]/form/div[1]/table/tbody/tr[2]/td[1]")).click(); 
-        Assert.assertEquals(driver.findElement(By.xpath("//textarea[@id='E0']")).getAttribute("value"),"He is handsome","Fail");
+             }
+         
+        //.............................Assert 2//
+         for(int j=1;j<2;j++)
+             {
+                 int r=4;
+         WebElement select3= TestCaseBase.driver.findElement(By.xpath(" /html/body/header/div/div["+r+"]/div/div/div["+j+"]/table/tbody/tr[2]/td[1]"));
+                 Assert.assertNotNull(select3,"Fail");
+                WebElement select4= TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[1]/img"));
+                  Assert.assertNotNull(select4,"Fail");
+                 WebElement select5= TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[2]/img"));
+                 Assert.assertNotNull(select5,"Fail");
+                 Assert.assertEquals(driver.findElement(By.cssSelector("select[disabled]")).getAttribute("disabled"), "true","Fail");
+             }
+         
+        //.............................Assert 3//
+            for(int j=1;j<4;j++)
+             {
+                 int r=5;
+                 WebElement select6= TestCaseBase.driver.findElement(By.xpath(" /html/body/header/div/div["+r+"]/div/div/div["+j+"]/table/tbody/tr[2]/td[1]"));
+                 Assert.assertNotNull(select6,"Fail");
+                 WebElement select7 = TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[1]/img"));
+                 Assert.assertNotNull(select7,"Fail");
+                 WebElement select8 = TestCaseBase.driver.findElement(By.xpath("/html/body/header/div/div["+r+"]/div/div/div["+j+"]/div/h[2]/img"));
+                 Assert.assertNotNull(select8,"Fail");
+             }
+       
         
-        WebElement select2 = TestCaseBase.driver.findElement(By.xpath("//select[@id='A0']"));
-        Select select1=new Select(select2);
-        String number=select1.getFirstSelectedOption().getText();
-        Assert.assertEquals(number, "1","Fail");
-        logout();
+        pp.logoutButton.click();
+
+
     }
 };
     
---
+++ 2-AutomationTest/PerficientTalentReviewSystemTests/src/main/java/com/perficient/test/pages/PMOleadPage.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.perficient.test.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ *
+ * @author bootcamp19
+ */
+public class PMOleadPage {
+
+    @FindBy(id="pmo2")
+    public static WebElement pmo2;
+
+    
+    
+    @FindBy(id="pmo0")
+    public static WebElement pmo0;
+
+    @FindBy(xpath="/html/body/header/div/div[3]/div/div/div/table/tbody/tr[2]/td[1]")
+    public static WebElement revid00;
+    
+    @FindBy(xpath="/html/body/header/div/div[3]/div/div/div/div/h[1]/img")
+    public static WebElement personal00;
+    @FindBy(xpath="/html/body/header/div/div[3]/div/div/div/div/h[2]/img")
+    public static WebElement supportive00;
+    
+    @FindBy(id="pmo1")
+    public static WebElement pmo1;
+
+    @FindBy(xpath="/html/body/header/div/div[4]/div/div/div/table/tbody/tr[2]/td[1]")
+    public static WebElement revid01;
+    @FindBy(xpath="/html/body/header/div/div[4]/div/div/div/div/h[1]/img")
+    public static WebElement personal01;
+    @FindBy(xpath="/html/body/header/div/div[4]/div/div/div/div/h[2]/img")
+    public static WebElement supportive01;
+    
+    
+    
+    
+    
+    
+//    @FindBy(id="revid1")
+//    public static WebElement click4Element;
+//    @FindBy(id="P1")
+//    public static WebElement click5Element;
+//    @FindBy(id="S1")
+//    public static WebElement click6Element;
+//    
+//    @FindBy(id="revid2")
+//    public static WebElement click7Element;
+//    @FindBy(id="P2")
+//    public static WebElement click8Element;
+//    @FindBy(id="S2")
+//    public static WebElement click9Element;
+    
+    
+    //********************US04 find Id**************************************************
+    @FindBy(id="logout")
+    public static WebElement logoutButton;
+}
